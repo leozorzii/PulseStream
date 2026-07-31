387125b84136a101722284c66a92057fbcb0035b
leozorzii <l.zorzi@ufn.edu.br>
2026-07-31 14:09:51 -0300

    feat(analytics): add sentiment classification and unit tests

--- apps/analytics/sentiment.py
+++ apps/analytics/sentiment.py
@@ -1,6 +1,10 @@
 import re
 from collections import Counter  # lib de contagem de itens numa lista
 
+STOPWORDS = {"o", "a", "de", "e", "no", "na", "é", "que", "do", "da"}
+POSITIVAS = {"bom", "ótimo", "adorei", "maravilhoso", "excelente", "amei", "incrível", "gostei"}
+NEGATIVAS = {"ruim", "péssimo", "odiei", "horrível", "terrível", "lixo", "detestei", "horrivel"}
+NEGACOES = {"não", "nao", "nunca", "nem", "jamais"}
 
 def limpar_texto(texto):
     """Remove tags HTML e normaliza o texto para minúsculas.
@@ -16,7 +20,6 @@ def limpar_texto(texto):
     return response
 
 
-STOPWORDS = {"o", "a", "de", "e", "no", "na", "é", "que", "do", "da"}
 
 
 def extrair_palavras_chave(texto):
@@ -40,3 +43,48 @@ def extrair_palavras_chave(texto):
     mais_comuns = contador_palavras.most_common(5)
     response = [par[0] for par in mais_comuns]
     return response
+
+def classificar_sentimento(texto):
+    
+    """Extrai a classificação do sentimento.
+    
+        Args:
+            texto (str): Texto a ser analisado.
+    
+        Returns:
+            texto(str): classificação do sentimento em "positivo", "negativo" ou "neutro"
+        """
+    
+    texto_limpo = limpar_texto(texto)
+    palavras = texto_limpo.split()
+    
+    positivas = 0
+    negativas = 0
+    negar = False
+    
+    #flag que verifica se a frase é positiva negativa. Ex: Não é bom 
+    for palavra in palavras:
+        if palavra in NEGACOES:
+            negar = True
+            continue
+    
+        if palavra in POSITIVAS:
+            if negar:
+                negativas += 1
+            else:
+                positivas += 1
+            negar = False
+            
+        if palavra in NEGATIVAS:
+            if negar:
+                positivas += 1
+            else:
+                negativas += 1
+            negar = False
+            
+    if positivas > negativas:
+        return "positivo"
+    elif negativas > positivas:
+        return "negativo"
+    else:
+        return "neutro"
