b39978bad336e1f20683fb62eb0eaaeb8c80bf5d
leozorzii <l.zorzi@ufn.edu.br>
2026-08-04 12:14:29 -0300

    feat(stream_core): add get_unprocessed_posts selector

--- apps/stream_core/selectors.py
+++ apps/stream_core/selectors.py
@@ -1,5 +1,6 @@
 from apps.stream_core.models import ContentSource
 from django.core.exceptions import ValidationError
+from apps.stream_core.models import RawPost
 
 def get_active_sources():
     """Metodo que retorna todas as fontes ativas
@@ -9,3 +10,12 @@ def get_active_sources():
         QuerySet[ContentSource]: fonte com is_active=True
     """
     return ContentSource.objects.filter(is_active=True)
+
+
+def get_unprocessed_posts():
+    """Metodo que retorna os posts nao processados, ordenados do mais antigo ao mais novo
+
+    Returns:
+        QuerySet[ContentSource]: post com is_processed = false, ordenados por published_at
+    """ 
+    return RawPost.objects.filter(is_processed=False).order_by("published_at")
--- tests/stream_core/test_selectors.py
+++ tests/stream_core/test_selectors.py
@@ -1,7 +1,10 @@
 import pytest
+from django.utils import timezone
 from apps.stream_core.services import create_content_source
 from apps.stream_core.selectors import get_active_sources
+from apps.stream_core.selectors import get_unprocessed_posts
 from apps.stream_core.models import ContentSource
+from apps.stream_core.models import RawPost
 
 
 
@@ -18,3 +21,32 @@ def test_get_active_source_atives():
     #so a ativa deve vir
     assert response.count() == 1
     assert response.first().name == "Ativa" 
+    
+@pytest.mark.django_db
+def test_get_unprocessed_posts_returns_only_unprocessed():
+    #Arrange - primeiro a fonte por que os posts precisam dela
+    fonte = create_content_source(name="Canal", plataform="YOUTUBE", external_id="UC_test")
+    
+    #para um post nao processado
+    RawPost.objects.create(
+        source = fonte,
+        external_id = "post_pending",
+        text_content = "new_coment",
+        published_at = timezone.now(),
+        is_processed = False,
+    )
+    
+    #para post ja processado
+    RawPost.objects.create(
+        source = fonte,
+        external_id = "post_processed",
+        text_content = "old_coment",
+        published_at = timezone.now(),
+        is_processed = True,
+     )
+    #Act
+    response = get_unprocessed_posts()
+    
+    #assert
+    assert response.count() == 1
+    assert response.first().external_id == "post_pending"
